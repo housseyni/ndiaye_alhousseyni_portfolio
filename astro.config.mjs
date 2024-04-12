ff354e996472d9f977586f4f81de3da970b270a0
housseyni <alhousseyni.ndiaye@outlook.fr>
2024-04-12 14:02:05 +0200

    astroconfig modifdre

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,5 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://housseyni.github.io",
-  sitemap: {
-    hostname: "https://housseyni.github.io",
-  },
-  build: {
-    outDir: "dist",
-  },
-  pages: {
-    src: "src/pages/**/*.astro",
-  },
-  public: {
-    src: ["public"],
-  },
-  projectRoot: ".",
-  // Définir la base à la racine du domaine
   base: "/",
 });
