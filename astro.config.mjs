c1528ed0e647973422b3aabb506aa0b46f4a2142
housseyni <alhousseyni.ndiaye@outlook.fr>
2024-04-12 13:49:01 +0200

    astro config modif

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,20 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://housseyni.github.io',
-  base: '/',
-})
+  site: "https://housseyni.github.io",
+  sitemap: {
+    hostname: "https://housseyni.github.io",
+  },
+  build: {
+    outDir: "dist",
+  },
+  pages: {
+    src: "src/pages/**/*.astro",
+  },
+  // Public folder is used to serve static files that don’t need to be processed by Astro. This can include anything in the `public` directory, such as images, fonts, or other assets.
+  public: {
+    src: ["public"],
+  },
+  
+}); 
+
